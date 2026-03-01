2fa476b5b6ea6b269205e737f9c6aff7b4a5aa3e
Tomas Balvin <balvintomas@gmail.com>
2026-03-01 01:49:12 +0100

    update format

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,7 +3,7 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  '*.{ts,js,json,md}': 'oxfmt',
+  '*.{ts,js,json,md,yml}': 'oxfmt',
   '*.{ts,js}': 'oxlint --type-aware --fix',
   // Do not pass any files into the build command
   'src/**/*.ts': () => 'bun run build',
